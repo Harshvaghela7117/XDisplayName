7cd72b31e4e71d700b7707f81038ce6ffa3e9b09
Harsh Vaghela <harshvaghela7117@gmail.com>
2024-03-02 22:03:03 +0530

    gnrs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import FullName from './Name/FullName';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [firstName, setFirstName] = useState("");
+  const handleChange = (event) => setFirstName(event.target.value);
+  const [lastName, setLastName] = useState("");
+  const handleChange2 = (event) => setLastName(event.target.value);
+  const [fullName, setFullName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+    if (firstName.trim() !== "" && lastName.trim() !== "") {
+      setFullName(`${firstName.trim()} ${lastName.trim()}`);
+    }
+  };
 
   return (
     <div className="App">
-      <FullName />
-    </div>
-  );
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="FirstName" value={firstName} onChange={handleChange} />
+        <input type="text" placeholder="LastName" value={lastName} onChange={handleChange2} />
+        <button type="submit">Submit</button>
+      </form>
+      {fullName && <h2>Full Name: {fullName}</h2>}
+ </div>
+);
 }
-
-export default App;
